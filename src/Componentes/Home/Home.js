c52e482e6405eb90ab6a604a5d79d9fe40d1945a
unknown <dasilvadeoliveirafabio@gmail.com>
2022-06-24 03:14:57 -0300

    parte das mensagens adaptada

--- src/Componentes/Home/Home.js
+++ src/Componentes/Home/Home.js
@@ -6,9 +6,10 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import ResponsiveAppBar from '../AppBar/ResponsiveAppBar';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ToggleButtonNotEmpty from '../SideBar/ToggleButtonNotEmpty';
-import { collection, onSnapshot, orderBy, query, where } from 'firebase/firestore';
+import { collection,where, onSnapshot, orderBy, query } from 'firebase/firestore';
 import db from '../../fireBaseConfig';
 import InputBase from '../imputMensagens/InputBase';
+
 export default function Home() {
   const auth = getAuth();
   const [user] = useAuthState(auth);
@@ -19,27 +20,44 @@ export default function Home() {
   const [idDaMensagem,setIdDaMensagem]=useState()
   useEffect(()=>{
     const mensageRef = query(collection(db,"chats"),where("sala","==",room),orderBy('uid'))
+    
     onSnapshot(mensageRef,snap=>{
       let array=[]
       snap.docs.forEach(elem=>{
+       // console.log(elem._key.path.segments[6])
+       //console.log(elem._key)
         array.push(elem.data())
-        console.log(elem.data().uid+' sala: '+elem.data().sala)
       })
       setMensagens(array)
+      console.log()
       let arrayZerado = array.length
       if (arrayZerado === 0) {
-        
         setIdDaMensagem(1)
-        console.log('ultimoId')
       }else{
         let ultimoId = array[array.length-1].uid  
         setIdDaMensagem(parseInt(ultimoId) + 1)
-        console.log(array.length)
       }
-     
-      
     })
-    
+  
+  //  async function criar(params) {
+  //     const docData = {
+  //       stringExample: "Hello world!",
+  //       booleanExample: true,
+  //       numberExample: 3.14159265,
+  //       dateExample: Timestamp.fromDate(new Date("December 10, 1815")),
+  //       arrayExample: [5, true, "hello"],
+  //       nullExample: null,
+  //       objectExample: {
+  //           a: 5,
+  //           b: {
+  //               nested: "foo"
+  //           }
+  //       }
+  //   };
+  //   await setDoc(doc(db, "data", "one"), docData);
+  //   }
+
+  //   criar()
   
     setTimeout(() => {
       uteis.scroll()
@@ -76,7 +94,8 @@ export default function Home() {
                                     } src={elem.photoURL} alt=''/>
                                   </div>
                                   <div>
-                                    <div className='mensagemItem'>{elem.mensagem}
+                                    <div onClick={()=>alert(elem.uid)} className='mensagemItem'>{elem.mensagem}
+                                       
                                        <div className={
                                           elem.usuarioLogado === user.displayName ?
                                           "dataLeft ":
