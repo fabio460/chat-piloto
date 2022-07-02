3f7f309d666ea92df1214a10dd4801b7c49dac13
unknown <dasilvadeoliveirafabio@gmail.com>
2022-07-02 16:59:51 -0300

    bug removido

--- src/Componentes/SideBar/FloatingActionButtonZoom.js
+++ src/Componentes/SideBar/FloatingActionButtonZoom.js
@@ -104,7 +104,7 @@ export default function FloatingActionButtonZoom({user,setVisivel,setRoom,getIdR
         bgcolor: 'background.paper',
         width: "100%",
         position: 'relative',
-        height: 490,
+        height: 450,
         margin:"auto"
       }}
     >
